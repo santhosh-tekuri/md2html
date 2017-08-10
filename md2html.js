92fb53007283c8005325c61334e7590bf0b4f47b
Santhosh Kumar Tekuri <santhosh.tekuri@gmail.com>
2017-08-10 18:16:07 +0530

    set page title from first <h1> content

--- md2html.js
+++ md2html.js
@@ -2,6 +2,14 @@ showdown.setOption('requireSpaceBeforeHeadingText', true);
 converter = new showdown.Converter();
 document.body.innerHTML = converter.makeHtml(document.body.innerText);
 
+// set page title from first <h1> content
+h1Elements = document.body.getElementsByTagName('h1');
+if(h1Elements.length>0) {
+    title = document.createElement('title');
+    title.innerText = h1Elements[0].innerText;
+    document.head.append(title)
+}
+
 link = document.createElement('link');
 link.rel = 'stylesheet';
 link.href = chrome.extension.getURL('theme.css');
