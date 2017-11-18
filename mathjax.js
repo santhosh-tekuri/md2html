77dd81b65f1b9422efb29faf552ed64a502d21a3
Santhosh Kumar Tekuri <santhosh.tekuri@gmail.com>
2017-11-18 23:19:13 +0530

    configuration for mathjax

--- mathjax.js
+++ mathjax.js
@@ -1,3 +1,14 @@
+/*
+// configuration for mathjax
+config = document.createElement('script');
+config.type = 'text/x-mathjax-config';
+config.textContent =
+    "MathJax.Hub.Config({\n" +
+    "  tex2jax: { inlineMath: [['$','$'], ['\\\\(','\\\\)']] }\n" +
+    "});";
+document.body.append(config);
+*/
+
 script = document.createElement('script');
 script.type = 'text/javascript';
 script.src = chrome.extension.getURL('MathJax/MathJax.js')+'?config=TeX-AMS-MML_HTMLorMML';
